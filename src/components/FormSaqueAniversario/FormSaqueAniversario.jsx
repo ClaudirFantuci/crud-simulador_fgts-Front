0a1611abe1f7dd1178948254e51f8aca491b5577
Claudir Fantuci <claudirfantucijunior@gmail.com>
2026-02-01 15:30:39 -0300

    feat: adicionada a listagem de simulações salvas no banco e ajustes no estilo

--- src/components/FormSaqueAniversario/FormSaqueAniversario.jsx
+++ src/components/FormSaqueAniversario/FormSaqueAniversario.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './FormSaqueAniversario.css';
+import SaqueAniversarioService from '../../service/SaqueAniversarioService';
 
 const MESES = [
     { label: 'Janeiro', value: 1 },
@@ -16,26 +17,75 @@ const MESES = [
     { label: 'Dezembro', value: 12 }
 ];
 
+
 const FormSaqueAniversario = ({ onSubmit }) => {
-    const [dados, setDados] = useState({
+    const [data, setData] = useState({
         nome: '',
         saldoFgts: '',
         mesAniversario: ''
     });
+    const [loading, setLoading] = useState(false);
+    const [result, setResult] = useState(null);
+    const [erro, setErro] = useState(null);
+    const [loadedList, setLoadedList] = useState([]);
 
+    useEffect(() => {
+        loadList();
+    }, []);
+
+    const loadList = async () => {
+        try {
+            setLoading(true);
+            const data = await SaqueAniversarioService.listAll();
+            setLoadedList(data);
+            setErro(null);
+        } catch (error) {
+            setErro(error.message)
+        } finally {
+            setLoading(false);
+        }
+    };
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setDados(prev => ({ ...prev, [name]: value }));
+        setData(prev => ({ ...prev, [name]: value }));
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!dados.mesAniversario || !dados.nome || !dados.saldoFgts) {
+        if (!data.mesAniversario || !data.nome || !data.saldoFgts) {
             alert('Preencha todos os campos obrigatórios');
             return;
         }
-        onSubmit(dados);
+        setLoading(true);
+        setErro(null);
+
+        try {
+            const sendData = {
+                nome: data.nome,
+                saldoFgts: parseFloat(data.saldoFgts),
+                mesAniversario: parseInt(data.mesAniversario)
+            };
+
+            const response = await SaqueAniversarioService.create(sendData);
+
+            setResult(response);
+
+            setData({
+                nome: "",
+                saldoFgts: "",
+                mesAniversario: "",
+            });
+
+            await loadList();
+
+        } catch (error) {
+            console.error("Erro ao calcular:", error)
+        } finally {
+            setLoading(false);
+        }
     };
+
+
 
     return (
         <div className="formContainer">
@@ -51,10 +101,11 @@ const FormSaqueAniversario = ({ onSubmit }) => {
                             className="inputField"
                             type="text"
                             name="nome"
-                            value={dados.nome}
+                            value={data.nome}
                             onChange={handleChange}
                             placeholder="Digite seu nome"
                             required
+                            disabled={loading}
                         />
                     </div>
 
@@ -63,9 +114,10 @@ const FormSaqueAniversario = ({ onSubmit }) => {
                         <select
                             className="selectField"
                             name="mesAniversario"
-                            value={dados.mesAniversario}
+                            value={data.mesAniversario}
                             onChange={handleChange}
                             required
+                            disabled={loading}
                         >
                             <option value="">Selecione o mês</option>
                             {MESES.map(mes => (
@@ -83,19 +135,49 @@ const FormSaqueAniversario = ({ onSubmit }) => {
                                 className="inputField"
                                 type="number"
                                 name="saldoFgts"
-                                value={dados.saldoFgts}
+                                value={data.saldoFgts}
                                 onChange={handleChange}
                                 placeholder="0,00"
                                 required
+                                disabled={loading}
                             />
                         </div>
                     </div>
 
-                    <button type="submit" className="buttonCalc">
-                        Calcular
+                    <button type="submit" className="buttonCalc" disabled={loading}>
+                        {loading ? 'Calculando...' : "Calcular"}
                     </button>
                 </form>
             </div>
+            {result && (
+                <div className="result-card">
+                    <h3>Resultado da Simulação</h3>
+                    <p><strong>Nome:</strong> {result.nome}</p>
+                    <p><strong>Saldo FGTS:</strong> R$ {result.saldoFgts?.toFixed(2)}</p>
+                    <p><strong>Aliquota:</strong> {result.faixa * 100}%</p>
+                    <p><strong>Valor Disponível:</strong> R$ {result.valorDisponivel?.toFixed(2)}</p>
+                    <p><strong>Mês:</strong> Saque disponível a partir do primeiro dia útil de {MESES.find(m => m.value === result.mesAniversario)?.label} por um período de 90 dias</p>
+                </div>
+            )}
+            {loadedList && (
+                <div className='simulation-list'>
+                    <h2>Simulações anteriores</h2>
+                    <div className='simulationGrid'>
+                        {Object.entries(loadedList).map(([key, item]) => (
+                            <div key={key} className='result-card'>
+                                <p><strong>Nome:</strong> {item.nome}</p>
+                                <p><strong>Saldo FGTS:</strong> R$ {item.saldoFgts?.toFixed(2)}</p>
+                                <p><strong>Aliquota:</strong> {item.faixa * 100}%</p>
+                                <p><strong>Valor Disponível:</strong> R$ {item.valorDisponivel?.toFixed(2)}</p>
+                                <p><strong>Mês:</strong> Saque disponível a partir do primeiro dia útil de {MESES.find(m => m.value === item.mesAniversario)?.label} por um período de 90 dias</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )
+            }
+
+
         </div>
     );
 };
---
+++ src/service/SaqueAniversarioService.js
@@ -0,0 +1,29 @@
+import api from "../config/api";
+
+class SaqueAniversarioService {
+    constructor() {
+        this.endPoint = '/api/SaqueAniversario';
+    };
+
+    async create(data) {
+        const response = await api.post(this.endPoint, data);
+        return response.data;
+    }
+
+    async listAll() {
+        const response = await api.get(this.endPoint);
+        return response.data;
+    }
+
+    async update(id, data) {
+        const response = await api.put(`${this.endPoint}/${id}`, data);
+        return response.data;
+    }
+
+    async delete(id) {
+        const response = await api.delete(`${this.endPoint}/${id}`);
+        return response.data;
+    }
+}
+
+export default new SaqueAniversarioService();
